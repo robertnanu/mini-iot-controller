9f7b7b8b92e53a9fbb8cda4f7dda3de65dcd2cdc
Nanu Robert <robertnanu@yahoo.com>
2022-02-03 12:46:01 +0200

    Add 4 more APIs

--- robert/temperature/api/serializers.py
+++ robert/temperature/api/serializers.py
@@ -4,4 +4,5 @@ from temperature.models import Device
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
-        fields = ['name', 'online', 'temperature', 'owner']
+        fields = ['id', 'name', 'online', 'temperature', 'owner']
+        # fields = ['temperature']
--- robert/temperature/api/views.py
+++ robert/temperature/api/views.py
@@ -23,18 +23,55 @@ class DeviceViewset(viewsets.ModelViewSet):
         return devices
 
     # Endpoint for filtering only by owner id
-    # def retrieve(self, request, *args, **kwargs):
-    #     params = kwargs
-    #     print(params['pk'])
-    #     devices = Device.objects.filter(owner=params['pk'])
-    #     serializer = DeviceSerializer(devices, many=True)
-    #     return Response(serializer.data) 
-    
     def retrieve(self, request, *args, **kwargs):
         params = kwargs
         print(params['pk'])
-        params_list = params['pk'].split('-')
-        devices = Device.objects.filter(
-            owner=params_list[0], name=params_list[1])
+        devices = Device.objects.filter(pk=params['pk'])
         serializer = DeviceSerializer(devices, many=True)
         return Response(serializer.data) 
+    
+    # def retrieve(self, request, *args, **kwargs):
+    #     params = kwargs
+    #     print(params['pk'])
+    #     params_list = params['pk'].split('-')
+    #     devices = Device.objects.filter(
+    #         owner=params_list[0], name=params_list[1])
+    #     serializer = DeviceSerializer(devices, many=True)
+    #     return Response(serializer.data) 
+
+    # def create(self, request, *args, **kwargs):
+    #     device_data = request.data
+
+    #     new_device = Device.objects.create(name=device_data['name'], temperature=device_data['temperature'],
+    #                                         online=device_data['online'], owner=int(device_data['owner']))
+        
+    #     new_device.save()
+
+    #     serializer = DeviceSerializer(new_device)
+
+    #     return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        loggedin_user = request.user
+        if loggedin_user == 'admin':
+            device = self.get_object()
+            device.delete()
+            response_message = {'message': "Item has been deleted"}
+        else:
+            response_message = {'message': 'Not Allowed'}
+
+        return Response(response_message)
+
+    def put(self, request, *args, **kwargs):
+        device = Device.objects.get()
+
+        data = request.data
+
+        device.name = data['name']
+        device.temperature = data['temperature']
+        device.online = data['online']
+        
+        device.save()
+
+        serializer = DeviceSerializer(device)
+        return Response(serializer.data)
